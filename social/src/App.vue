77f12d19ab591311a065df6ac6eb728cd8bef355
leandroalvesmachado <leandroalvesmachado@gmail.com>
2020-04-21 19:23:43 -0300

    Subindo front-end

---
+++ social/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+    <span>
+        <header>
+            <app-navbar
+                logo="Social"
+                url="#"
+                cor="green darken-1"
+            />
+        </header>
+        <main>
+            <div class="container">
+                <div class="row">
+                    <app-grid tamanho="4">Menu</app-grid>
+                    <app-grid tamanho="8">
+                        <router-view />
+                    </app-grid>
+                </div>
+            </div>
+        </main>
+        <app-footer
+            cor="green darken-1"
+            logo="Social"
+            descricao="Teste"
+            ano="2020"
+        >
+            <li><a class="grey-text text-lighten-3" href="#!">Home</a></li>
+            <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+            <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+            <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+        </app-footer>
+    </span>
+</template>
+
+<script>
+
+import Navbar from '@/components/layouts/navbar';
+import Footer from '@/components/layouts/footer';
+import Grid from '@/components/layouts/grid';
+
+export default {
+    name: "App",
+    components: {
+        'app-navbar': Navbar,
+        'app-footer': Footer,
+        'app-grid': Grid
+    }
+};
+</script>
+
+<style>
+</style>
---
+++ social/src/components/layouts/navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav :class="cor || 'blue'">
+        <div class="nav-wrapper container">
+            <a :href="url || '#'" class="brand-logo">{{ logo || 'Site' }}</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">JavaScript</a></li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    props: [
+        'logo',
+        'url',
+        'cor'
+    ],
+    data () {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
